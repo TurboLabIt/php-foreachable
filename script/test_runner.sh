348145d25fe16fccc610c27102d9202e1ef567a2
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-08-12 00:55:16 +0200

    new testing script

--- script/test_runner.sh
+++ script/test_runner.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-SCRIPT_NAME=test_runner
+## Symfony Bundle Tester https://github.com/TurboLabIt/symfony-bundle-builder
+# 📚 RTFM: https://github.com/TurboLabIt/symfony-bundle-builder/blob/main/README.md
 
-if [ ! -d "vendor" ]; then
-  XDEBUG_MODE=off composer install
+clear
+
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+
+SBB_INSTALL_DIR=/usr/local/turbolab.it/symfony-bundle-builder/
+SBB_RAW_REPO_URL=https://raw.githubusercontent.com/TurboLabIt/symfony-bundle-builder/main/
+
+if [ -f "${SBB_INSTALL_DIR}scripts/symfony-bundle-tester.sh" ]; then
+  source "${SBB_INSTALL_DIR}scripts/symfony-bundle-tester.sh"
+else
+  source <(curl -s "${SBB_RAW_REPO_URL}scripts/symfony-bundle-tester.sh")
 fi
-
-XDEBUG_MODE=off ${PHP_CLI} ./vendor/bin/phpunit tests
-
-echo ""
